8a6276c89e1b736726dae54b28261ede80158f65
mvieira4 <47149053+mvieira4@users.noreply.github.com>
2020-12-08 13:44:45 -0500

    Final Report Submission

--- firmware/Pattern Generator/main.c
+++ firmware/Pattern Generator/main.c
@@ -23,61 +23,81 @@ static uint8_t buttonPress = 0;
 static uint8_t bit = 0;
 static uint8_t channelNum = 0;
 static uint8_t voltage = 0;
-static uint8_t channel[8] = {0x65,0b01010111,0b00111011,0xf6,0x82,0x00,0x34,0b01010101};
+static uint8_t channel[8] = {0b11110000,0b11001100,0b10101010,0b11001111,0b11001111,0x22,0x34,0x22};
 
 int main(void)
 {
 	cli();
 
 	DDRB = 0b11111110;
-	PORTB = 0b11111111;
+	PORTB = 0b11111110;
 	
-	DDRC = 0b00000000;
-	PORTC = 0b11111111;
+	DDRC = 0b11111110;
+	PORTC = 0b11111110;
 
 	DDRD = 0b01111111;
 	PORTD = 0b01111111;
  
 	DDRE = 0b11111111;
-	PORTE = 0b11111110;
+	PORTE = 0b11111111;
 
  	PCICR = 0b00000111;
  	PCMSK0 = 0b00000001;
  	PCMSK1 = 0b00000001;
 	PCMSK2 = 0b10000000;
 
-    OCR1A = 0x0FFF;
+    OCR1A = 0xFF00;
     TCCR1B = (1 << WGM12);
     TIMSK1 = (1 << OCIE1A);
     TCCR1B = (0b11 << CS10);
 	
 	sei();
 	
+	_delay_ms(1000);
+	
     while (1) 
     {	
 		
 		switch (mode)
 		{
-			case 0:
-				channel[channelNum] = (channel[channelNum] + rotorCheck()) & 0b00111111;
-				PORTD=~(channel[channelNum]);
-				break;
-
-			case 1:
-				channelNum = (channelNum + rotorCheck()) % 8;
-				PORTD=~(channelNum);
-				break;
-
-			case  2:
+			case  0:
 				voltage = (voltage + rotorCheck()) % 2;
 				if (voltage == 0)
 				{
-					PORTD = 0b11101111;
+					PORTB = 0b11100001;
+					PORTE = 0b11111110;
+					_delay_ms(250);
+					PORTB=0xFF;
+					PORTE=0xFF;
+					_delay_ms(250);
+					PORTD&=0b10111111;
 				}
 				else
 				{
-					PORTD = 0b11011111;
+					PORTB = 0b11110001;
+					PORTE = 0b11111111;
+					_delay_ms(250);
+					PORTB=0xFF;
+					PORTE=0xFF;
+					_delay_ms(250);
+					PORTD&=0b01000000;
 				}
+				break;
+			
+			case 1:
+				channel[channelNum] = (channel[channelNum] + rotorCheck()) & 0b11111111;
+				PORTB=~(channel[channelNum])<<1;
+				PORTE=~(channel[channelNum])>>4;
+				break;
+
+			case 2:
+				channelNum = (channelNum + rotorCheck()) % 8;
+				PORTB=~(1<<channelNum)<<1;
+				PORTE=~(1<<channelNum)>>4;
+				_delay_ms(250);
+				PORTB=0xFF;
+				PORTE=0xFF;
+				_delay_ms(250);
 				break;
 		}
 	}
@@ -109,33 +129,29 @@ ISR(PCINT0_vect)
 {	
 	if(!turned)
 	{
-	clockwise = 1;
+		clockwise = 1;
 	}
 	turned = 1;
-	_delay_ms(150);
+	_delay_ms(100);
 }
-
-
 
 ISR(PCINT1_vect)
-{
-	if(!turned)
-	{
-	clockwise = 0;
-	}
-	turned = 1;
-	_delay_ms(150);
-}
-
-
-
-ISR(PCINT2_vect)
 {
 	if (!buttonPress)
 	{
 	mode = (mode + 1) % 3;
 	}
 	buttonPress = ~buttonPress;
+}
+
+ISR(PCINT2_vect)
+{
+	if(!turned)
+	{
+	clockwise = 0;
+	}
+	turned = 1;
+	_delay_ms(100);
 }
 
 
@@ -151,7 +167,7 @@ ISR(TIMER1_COMPA_vect)
  	{
  		temp |= (((channel[i] >> (bit)) & 0b00000001) << i);
  	}
- 	PORTB = temp & 0b11111110;
+ 	PORTD = ~(temp & 0b00111111);
  	bit = (bit + 1) % 8;
 	 
 	sei();
